ca7b52d312f49e3c6f8ac11ecf9129a48bafccb5
KPryce <prycekevaughn@gmail.com>
2022-03-10 22:40:54 -0500

    Completed basic implementation of 'Add Patient Fact'

--- AI_Project.pl
+++ AI_Project.pl
@@ -10,8 +10,20 @@ get_patientn(A,B,C,D,E,F,G,H,I):- patientn(A,B,C,D,E,F,G,H,I).
 get_patientw(A,B,C,D,E,F,G,H,I,J,K):- patientw(A,B,C,D,E,F,G,H,I,J,K).
 get_punderlying(A,B):- punderlying(A,B).
 
-underlying_condition((empty)).
-ethnicity((empty)).
+underlying_condition('(empty)').
+underlying_condition(diabetes).
+underlying_condition(h).
+underlying_condition(l).
+underlying_condition(k).
+underlying_condition(p).
+underlying_condition(s).
+underlying_condition(d).
+underlying_condition(q).
+
+
+
+
+ethnicity('(empty)').
 %ethnicity('American').
 
 
--- AI_Project.py
+++ AI_Project.py
@@ -17,6 +17,8 @@ def add_fact():
         ethnicitytb.grid(row=0, column=1)
         esubmitbtn = Button(condition_window, text="Submit", command=sendEToDatabase)
         esubmitbtn.grid(row=1, column=0)
+        backbtn = Button(condition_window, text="Go Back", command=readd_fact)
+        backbtn.grid(row=1, column=1)
 
     def sendCToDatabase():
         cond = conditiontb.get()
@@ -39,6 +41,12 @@ def add_fact():
         conditiontb.grid(row=0, column=1)
         csubmitbtn = Button(condition_window, text="Submit", command=sendCToDatabase)
         csubmitbtn.grid(row=1, column=0)
+        backbtn = Button(condition_window, text="Go Back", command=readd_fact)
+        backbtn.grid(row=1, column=1)
+
+    def readd_fact():
+        condition_window.destroy()
+        add_fact()
 
     # Window
     condition_window = Toplevel()
@@ -62,9 +70,13 @@ def add_fact():
     ethnicitybtn.grid(row=1, column=0)
 
 
-def add_reg():
+def add_patient():
+    global gender
+    global ans
+    global clicked
+
     # Inner Commands
-    def add_reg_patient():
+    def add_patient_to_prolog():
         pname = name.get().capitalize()
         page = age.get()
         pheight1 = height1.get()
@@ -90,6 +102,17 @@ def add_reg():
             #
             # print(c)
 
+        conditions = []
+        length = len(CONDITIONS)
+        for con in range(length):
+            if variables[con].get() == "On":
+                conditions.append(str(CONDITIONS[con]).capitalize())
+
+        for f in (conditions):
+            prolog.assertz("punderlying('" + pname + "','" + f + "')")
+            c = list(prolog.query("get_punderlying('" + pname + "',B)"))
+            print(c)
+
     def display_pressure(frame, val):
         global systolic
         global diastolic
@@ -114,36 +137,36 @@ def add_reg():
                 diastolic.destroy()
 
     # Window
-    reg_window = Toplevel()
-    reg_window.title('Add Regular Covid Virus Fact')
-    reg_window.iconbitmap('./favicon.ico')
-    reg_window.configure(bg="#353535")
-    reg_window.geometry("850x600")
+    patient_window = Toplevel()
+    patient_window.title('Add Patient Fact')
+    patient_window.iconbitmap('./favicon.ico')
+    patient_window.configure(bg="#353535")
+    patient_window.geometry("850x600")
 
     # Frame
-    reg_frame = LabelFrame(reg_window, text="Condition Details", padx=40, pady=40, borderwidth=10,
-                           bg="#fff")
-    reg_frame.grid(row=0, column=0, padx=(80, 20), pady=100)
+    patient_frame = LabelFrame(patient_window, text="Condition Details", padx=40, pady=40, borderwidth=10,
+                               bg="#fff")
+    patient_frame.grid(row=0, column=0, padx=(80, 20), pady=100)
 
     # Labels
-    Label(reg_frame, text="Name").grid(row=0, column=0)
-    Label(reg_frame, text="Age").grid(row=1, column=0)
-    Label(reg_frame, text="Temperature (in Celsius)").grid(row=2, column=0)
-    Label(reg_frame, text="Height (Ft)").grid(row=3, column=0)
-    Label(reg_frame, text="Height (In)").grid(row=4, column=0)
-    Label(reg_frame, text="Ethnicity").grid(row=5, column=0)
-    Label(reg_frame, text="Gender").grid(row=6, column=0)
-    Label(reg_frame, text="Experienced dizziness, fainting or blurry vision?").grid(row=7, column=0)
-    Label(reg_frame, text="Weight").grid(row=8, column=0)
-    # Label(reg_frame, text="Underlying condition").grid(row=5, column=0)
+    Label(patient_frame, text="Name").grid(row=0, column=0)
+    Label(patient_frame, text="Age").grid(row=1, column=0)
+    Label(patient_frame, text="Temperature (in Celsius)").grid(row=2, column=0)
+    Label(patient_frame, text="Height (Ft)").grid(row=3, column=0)
+    Label(patient_frame, text="Height (In)").grid(row=4, column=0)
+    Label(patient_frame, text="Ethnicity").grid(row=5, column=0)
+    Label(patient_frame, text="Gender").grid(row=6, column=0)
+    Label(patient_frame, text="Experienced dizziness, fainting or blurry vision?").grid(row=7, column=0)
+    Label(patient_frame, text="Weight").grid(row=8, column=0)
+    Label(patient_frame, text="Underlying condition").grid(row=11, column=0)
 
     # Text Boxes
-    name = Entry(reg_frame, width=30)
-    age = Entry(reg_frame, width=30)
-    temp = Entry(reg_frame, width=30)
-    height1 = Entry(reg_frame, width=30)
-    height2 = Entry(reg_frame, width=30)
-    weight = Entry(reg_frame, width=30)
+    name = Entry(patient_frame, width=30)
+    age = Entry(patient_frame, width=30)
+    temp = Entry(patient_frame, width=30)
+    height1 = Entry(patient_frame, width=30)
+    height2 = Entry(patient_frame, width=30)
+    weight = Entry(patient_frame, width=30)
 
     name.grid(row=0, column=1, columnspan=2)
     age.grid(row=1, column=1, columnspan=2)
@@ -156,34 +179,49 @@ def add_reg():
     selected = StringVar()
     ETHNICITY = [e['X'] for e in list(prolog.query("ethnicity(X)"))]
     selected.set(ETHNICITY[0])
-    ethnicities = OptionMenu(reg_frame, selected, *ETHNICITY)
+    ethnicities = OptionMenu(patient_frame, selected, *ETHNICITY)
     ethnicities.grid(row=5, column=1, columnspan=2)
 
-    # clicked = StringVar()
-    # CONDITIONS = [c['X'] for c in list(prolog.query("underlying_condition(X)"))]
-    # clicked.set(CONDITIONS[0])
-    # underlying = OptionMenu(reg_frame, clicked, *CONDITIONS)
-    # underlying.grid(row=5, column=1, columnspan=2)
+    # Checkbox
+    CONDITIONS = [c['X'] for c in list(prolog.query("underlying_condition(X)"))]
+    names = []
+    variables = []
+    for x in range(len(CONDITIONS)):
+        names.append("cb" + str(x))
+        variables.append(StringVar(value="clicked"+str(x)))
+    k = 0
+    i = 11
+    j = 1
+    for c in range(len(CONDITIONS)):
+        variables[k] = StringVar()
+        names[k] = Checkbutton(patient_frame, text=CONDITIONS[k], variable=variables[k], onvalue="On", offvalue="Off")
+        if j % 4 == 0:
+            i += 1
+            j = 1
+        names[k].grid(row=i, column=j)
+        names[k].deselect()
+        k += 1
+        j += 1
 
     # Radio Button
     gender = StringVar()
-    gender.set("Male")
-    Radiobutton(reg_frame, text="Male", variable=gender, value="male").grid(row=6, column=1)
-    Radiobutton(reg_frame, text="Female", variable=gender, value="female").grid(row=6, column=2)
+    gender.set("male")
+    Radiobutton(patient_frame, text="Male", variable=gender, value="male").grid(row=6, column=1)
+    Radiobutton(patient_frame, text="Female", variable=gender, value="female").grid(row=6, column=2)
 
     ans = StringVar()
     ans.set("no")
-    ansbtn1 = Radiobutton(reg_frame, text="Yes", variable=ans, value="yes",
-                          command=lambda: display_pressure(reg_frame, 0))
+    ansbtn1 = Radiobutton(patient_frame, text="Yes", variable=ans, value="yes",
+                          command=lambda: display_pressure(patient_frame, 0))
     ansbtn1.grid(row=7, column=1)
-    ansbtn2 = Radiobutton(reg_frame, text="No", variable=ans, value="no",
-                          command=lambda: display_pressure(reg_frame, 1))
+    ansbtn2 = Radiobutton(patient_frame, text="No", variable=ans, value="no",
+                          command=lambda: display_pressure(patient_frame, 1))
     ansbtn2.grid(row=7, column=2)
 
     radio_buttons = [ansbtn1, ansbtn2]
 
     # Submit Button
-    Button(reg_frame, text="Submit", justify=CENTER, command=add_reg_patient).grid(row=11, column=0)
+    Button(patient_frame, text="Submit", justify=CENTER, command=add_patient_to_prolog).grid(row=20, column=0)
 
 
 # Main Window
@@ -203,8 +241,7 @@ info_frame.grid(row=0, column=1, padx=(20, 80), pady=100)
 # Buttons
 Button(main_frame, text="Add Condition/Ethnicity Fact", command=add_fact).grid(row=0, column=0, pady=10, padx=100,
                                                                                ipadx=26)
-Button(main_frame, text="Add Patient Fact", command=add_reg).grid(row=2, column=0, pady=10, padx=100)
-
+Button(main_frame, text="Add Patient Fact", command=add_patient).grid(row=2, column=0, pady=10, padx=100)
 Button(main_frame, text="Diagnose Patient").grid(row=4, column=0, pady=10, padx=100, ipadx=33)
 Button(main_frame, text="Display Statistics").grid(row=5, column=0, pady=10, padx=100, ipadx=33)
 
